d945439b7357d7473560426ad265880ce5ad103f
rokylebrown <r.kyle.brown@gmail.com>
2021-04-03 00:15:02 -0400

    heroku push

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize('postgres://postgres:password123@localhost:5433/kyle_streamlytics')
+module.exports = new Sequelize('postgres://postgres:password123@localhost:5433/kyle_streamlytics') 
