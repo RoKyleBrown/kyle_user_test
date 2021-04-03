6e65aa66d3493d0d23e2fcee0baa207987dec2df
rokylebrown <r.kyle.brown@gmail.com>
2021-04-03 15:38:03 -0400

    PERN Stack2

--- config/prod_database.js
+++ config/prod_database.js
@@ -1,4 +1,5 @@
-// const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
+const { Client } = require('pg');
 
 // module.exports = new Sequelize(`${process.env.Database}`, `${process.env.User}`, 
 //     `${process.env.Password}`, {
@@ -7,3 +8,18 @@
 // });
 
 // module.exports = new Sequelize('postgres://robertkbrown:@localhost:5432/kyle_technical_test') 
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect();
+debugger;
+module.exports = new Sequelize(`${process.env.Database}`, `${process.env.User}`, 
+    `${process.env.Password}`, {
+        host: `${process.env.Host}`,
+    dialect: 'postgres'
+});
