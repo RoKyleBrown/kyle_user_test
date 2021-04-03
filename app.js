1dc38ed45b8db3a558a77efc3c8cc5b852ce5d33
rokylebrown <r.kyle.brown@gmail.com>
2021-04-03 13:51:38 -0400

    PERN Stack2

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('frontend/build'));
+//     app.get('/', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//     })
+// }
 
 app.get("/", (req, res) =>{
     res.send("Hello World!")
