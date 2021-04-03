46523dbe3fe859e8f09042d61e86b277fdb72da0
rokylebrown <r.kyle.brown@gmail.com>
2021-04-03 17:30:58 -0400

    heroku debug2

--- config/prod_database.js
+++ config/prod_database.js
@@ -1,20 +1,4 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-// module.exports = new Sequelize(`${process.env.Database}`, `${process.env.User}`, 
-//     `${process.env.Password}`, {
-//         host: `${process.env.Host}`,
-//     dialect: 'postgres'
-// });
 
-// module.exports = new Sequelize('postgres://robertkbrown:@localhost:5432/kyle_technical_test') 
-
-console.log(`${process.env.DB_DATABASE}`);
-console.log(`${process.env.DB_USERNAME}`);
-console.log(`${process.env.DB_HOST}`);
-
-module.exports = new Sequelize(`${process.env.DB_DATABASE}`, `${process.env.DB_USERNAME}`, 
-    `${process.env.DB_PASSWORD}`, {
-        host: `${process.env.DB_HOST}`,
-    dialect: 'postgres'
-});
+module.exports = new Sequelize(`${process.env.DATABASE_URL}`) 
