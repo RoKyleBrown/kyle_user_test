87329befdf3744af3d4a34294fa259598a88c30c
rokylebrown <r.kyle.brown@gmail.com>
2021-04-03 16:53:37 -0400

    PERN Stack2

--- config/prod_database.js
+++ config/prod_database.js
@@ -9,9 +9,12 @@ require('dotenv').config();
 
 // module.exports = new Sequelize('postgres://robertkbrown:@localhost:5432/kyle_technical_test') 
 
+console.log(`${process.env.DB_DATABASE}`);
+console.log(`${process.env.DB_USERNAME}`);
+console.log(`${process.env.DB_HOST}`);
 
 module.exports = new Sequelize(`${process.env.DB_DATABASE}`, `${process.env.DB_USERNAME}`, 
     `${process.env.DB_PASSWORD}`, {
-        host: `ec2-54-205-183-19.compute-1.amazonaws.com`,
+        host: `${process.env.DB_HOST}`,
     dialect: 'postgres'
 });
