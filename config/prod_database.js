252819cc694d43fb938e278ef2a637b7c755f921
rokylebrown <r.kyle.brown@gmail.com>
2021-04-03 18:08:09 -0400

    heroku debug5

--- config/prod_database.js
+++ config/prod_database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 const pg = require('pg');
-pg.defaults.ssl.require = true;
-pg.defaults.ssl.rejectUnauthorized = false;
+pg.defaults.ssl = true;
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 
 module.exports = new Sequelize(`${process.env.DATABASE_URL}`);
