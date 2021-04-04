d28c3e7930d6682e55e1a60752be0abbcebcab48
rokylebrown <r.kyle.brown@gmail.com>
2021-04-03 20:50:56 -0400

    service worker 1

--- config/database.js
+++ config/database.js
@@ -1,11 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    console.log(process.env);
-    console.log('in pro -kb');
     module.exports = require('./prod_database');
 } else {
     module.exports = require('./dev_database'); 
 }
-
-// const { Sequelize } = require('sequelize');
-
-// module.exports = new Sequelize('postgres://robertkbrown:@localhost:5432/kyle_technical_test') 
