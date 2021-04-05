a84deb10a3b81c3a6afa837d9fb7b904109a4dcd
rokylebrown <r.kyle.brown@gmail.com>
2021-04-05 02:51:28 -0400

    frame test

--- frontend/src/components/preload/logo_gif.jsx
+++ frontend/src/components/preload/logo_gif.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import loadGif from '../../assets/profiler-logo2.mp4'
+import loadGif from '../../assets/profiler-logo2.mp4';
+import logo from '../../assets/icon-frame0.png';
 
 const LogoGif = () => {
  
@@ -11,7 +12,7 @@ const LogoGif = () => {
             muted loop src={loadGif}
             />
 
-            <p style={{zIndex:"0", position:"absolute"}}>.</p>
+            <img style={{zIndex:"0", position:"absolute"}} src={logo}/>
 
         </div>
     )
