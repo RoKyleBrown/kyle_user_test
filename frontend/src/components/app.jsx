ec5b9354955cd163c6fdd4f1212dd3050b83ca6c
rokylebrown <r.kyle.brown@gmail.com>
2021-04-04 20:23:23 -0400

    background patt

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserShowContainer from './users/user_show_container';
 import { Switch, Route } from 'react-router-dom';
+import './app.css';
 
 const App = () => {
 
